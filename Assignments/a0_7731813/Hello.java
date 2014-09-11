4dd5404b61ff18aca9e57b65fef158059cdb8f67
Matthew Langlois <fletchto99@gmail.com>
2014-09-11 19:50:05 -0400

    Assignment 0 Lab 1 -- Basic hello world program

---
+++ Assignments/a0_7731813/Hello.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+
+/*
+ * Course: ITI1120
+ * Assignment 0
+ * Langlois, Matthew
+ * 7731813
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		out.println("Hello World!");
+	}
+
+}
