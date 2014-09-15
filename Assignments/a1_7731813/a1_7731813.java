4de91dc60e8dda97cbcc4982e18cbbc900c76b7b
Matthew Langlois <fletchto99@gmail.com>
2014-09-15 00:34:50 -0400

    Basic Error checking

--- Assignments/a1_7731813/a1_7731813.java
+++ Assignments/a1_7731813/a1_7731813.java
@@ -10,36 +10,48 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class a1_7731813 {
 
 	public static void main(final String[] args) throws ClassNotFoundException,
-	InstantiationException, IllegalAccessException,
-	UnsupportedLookAndFeelException {
+			InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
 
 		// Clean up the UI for any OS
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		final int option = Integer.parseInt(JOptionPane
 				.showInputDialog("Which question are you testing?"));
 		/*
-		 * ==================================================
-		 * Question 1
+		 * ================================================== Question 1
 		 * ==================================================
 		 */
-		//TODO: Error checking
-		switch (option) {
-			case 1:
-				final double farenheit = Double
-				.parseDouble(JOptionPane
-						.showInputDialog("What is the temperature in Farenheit?"));
-				JOptionPane
-				.showMessageDialog(
-						null,
-						String.format(
-								"%.1f degrees farenheit is equivlent to %.2f degrees kelvin.",
-								farenheit,
-								((farenheit - 32) / 1.8) + 273.15));
-				break;
-			default:
-				JOptionPane.showMessageDialog(null, "Invalid question entered");
-				break;
+		// TODO: Error checking
+		try {
+			switch (option) {
+				case 1:
+					farenheitToKelvin();
+					break;
+				default:
+					JOptionPane.showMessageDialog(null,
+							"Invalid question entered");
+					break;
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
 
+	}
+
+	public static final void farenheitToKelvin() throws Exception {
+		try {
+			final double farenheit = Double.parseDouble(JOptionPane
+					.showInputDialog("What is the temperature in Farenheit?"));
+			JOptionPane
+					.showMessageDialog(
+							null,
+							String.format(
+									"%.1f degrees farenheit is equivlent to %.3f degrees kelvin.",
+									farenheit,
+									((farenheit - 32) / 1.8) + 273.15));
+		} catch (Exception e) {
+			throw new Exception(
+					"You failed to enter a valid number for the temperature in farenheit.");
+		}
 	}
 }
