d8dd1659d22e12cb44ca3ddbe27c7502be14b0f3
Matthew Langlois <fletchto99@gmail.com>
2014-10-01 16:53:05 -0400

    just for fun, check execution times

--- Labs/Lab 4/L4Q6.java
+++ Labs/Lab 4/L4Q6.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 
-
 public class L4Q6 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.printf("The sum of squares is %d", sumOfSquares(s.nextInt()));
+        long start = System.currentTimeMillis();
+        System.out.printf("The sum of squares is %d%n",
+                sumOfSquares(s.nextInt()));
+        long execute = System.currentTimeMillis() - start;
+        System.out.println("Recursive: " + execute);
+
+        start = System.currentTimeMillis();
+        int sumOfSquares = 0;
+        int n = s.nextInt();
+        for (int i = n; i > 0; --i) {
+            sumOfSquares += Math.pow(i, 2);
+        }
+        execute = System.currentTimeMillis() - start;
+        System.out.println(sumOfSquares);
+        System.out.println("Iter: " + execute);
         s.close();
     }
 
     private static int sumOfSquares(int n) {
-        if (n ==1 ){
+        if (n == 1) {
             return 1;
         }
-        return (int) (sumOfSquares(n-1) + Math.pow(n,2));
+        return (int) (Math.pow(n, 2) + sumOfSquares(n - 1));
     }
 
 }
